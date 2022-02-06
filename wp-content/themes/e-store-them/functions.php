b9104cbebb7c889f46ec1cfd522130479db989c2
eds4444 <efimov0204@gmail.com>
2022-02-06 18:02:54 +0200

    finish lector loh

--- wp-content/themes/e-store-them/functions.php
+++ wp-content/themes/e-store-them/functions.php
@@ -1,5 +1,18 @@
 <?php
 
+add_action( 'after_setup_theme', 'crb_load' );
+
+function crb_load(){
+	load_template( get_template_directory(  ), '/includes/carbon-fields/vendor/autoload.php'); 
+	\Carbon_Fields\Carbon_Fields::boot();
+}
+
+add_action( 'carbon_fields_register_fields', 'estore_custom_fields');
+function estore_custom_fields(){
+	require get_template_directory(  ) . '/includes/custom-fields-options/metabox.php';
+    require get_template_directory(  ) . '/includes/custom-fields-options/theme-options.php';
+}
+
 require get_template_directory(  ) . '/includes/theme-settings.php';
 
 require get_template_directory(  ) . '/includes/widget-areas.php';
